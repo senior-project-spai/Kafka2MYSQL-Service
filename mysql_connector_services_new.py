a8c24ab9cfbc6eceedc6cb56e7acb552e48dc5eb
Supakorn Wongsawang <supakorn.wo@ku.th>
2020-02-15 01:17:44 +0700

    add age

--- mysql_connector_services_new.py
+++ mysql_connector_services_new.py
@@ -19,7 +19,8 @@ c = Consumer({
     'auto.offset.reset': 'earliest'
 })
 
-c.subscribe(['face-result-gender', 'face-result-race', 'test'])
+c.subscribe(['face-result-gender', 'face-result-race',
+             'test', 'face-result-age'])
 
 add_gender_query = ("INSERT INTO Gender "
                     "(face_image_id, type, confidence, position_top, position_right, position_bottom, position_left, time) "
@@ -105,10 +106,47 @@ def test(msg):
     print(msg)
 
 
+def add_age(msg):
+    msg_json = json.loads(msg)
+    print(msg)
+    mydb = mysql.connector.connect(
+        host=MYSQL_HOST,
+        user=MYSQL_USER,
+        passwd=MYSQL_PASS,
+        port=MYSQL_PORT,
+        database=MYSQL_DB,
+    )
+    data_to_update = {
+        'face_image_id': msg_json['face_image_id'],
+        'min_age': msg_json['min_age'],
+        'max_age': msg_json['max_age'],
+        'confidence': msg_json['confidence'],
+        'position_top': msg_json['position_top'],
+        'position_right': msg_json['position_right'],
+        'position_bottom': msg_json['position_bottom'],
+        'position_left': msg_json['position_left'],
+        'time': msg_json['time']
+    }
+    cursor = mydb.cursor()
+    error = False
+    try:
+        cursor.execute(add_race_query, data_to_update)
+    except (mysql.connector.Error) as e:
+        print(e)
+        error = True
+    mydb.commit()
+    cursor.close()
+    if not error:
+        print("Added")
+    mydb.close()
+    print()
+
+
 func_dict = {
     'face-result-gender': add_gender,
     'face-result-race': add_race,
-    'test': test
+    'test': test,
+    'face-result-age': add_age
 }
 
 print("SERVICE STARTED MYSQL_HOST:{}, KAFKA_HOST:{}, KAFKA_PORT:{}".format(
