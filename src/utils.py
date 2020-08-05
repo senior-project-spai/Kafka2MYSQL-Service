a6600dacc1a7b986db3408c7aa847e20d85b9d1b
Rawit Panjaroen <check.rawit@gmail.com>
2020-08-06 00:29:02 +0700

    fix: get value from dict

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,9 @@
 def find_intersect_area(r0, r1):
-    left = max(r1.left, r0.left)
-    right = min(r1.right, r0.right)
-    bottom = min(r1.bottom, r0.bottom)
-    top = max(r1.top, r0.top)
+    ''' find intersection area, return None if they are not intersect'''
+    left = max(r1["left"], r0["left"])
+    right = min(r1["right"], r0["right"])
+    bottom = min(r1["bottom"], r0["bottom"])
+    top = max(r1["top"], r0["top"])
 
     if (left < right) and (top < bottom):
         return (right - left) * (bottom - top)
